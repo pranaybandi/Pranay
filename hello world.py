d9b22185ee10cccac1df1acf5620eec1a454618f
pranaybandi <46145353+pranaybandi@users.noreply.github.com>
2019-01-14 15:26:27 -0500

    Add files via upload
    hello world

---
+++ hello world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# this is sample file just for beginners
+print('pranay')
+day='monday'
+print (day)
+if day == 'monday':
+  print("today is "+day)
+  a=10
+  print('the number is ',a)
+  print("hello class")
+else:
+    print("today is not "+day)
+
+
+a=b=c=100
+print('the number is ' ,a,b,c)
+print (a+b)
+print (a/b)
+print(a*b)
+print(a%b)
